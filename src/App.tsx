ee66feddb85f29401c3c5688314d02408170fc12
Shahabas Paramban <shahabaspm500@gmail.com>
2023-11-26 23:46:07 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+
+  return (
+    <h1 className="p-5 text-8xl bg-PrimaryColor h-screen text-Black font-Gravitica  font-medium">Gravitica</h1>
+  )
+}
+
+export default App
+// 171717
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'PrimaryColor': '#e2dfdc',
+      'SecondaryColor': '#a6a6a6',
+      'TertiaryColor': '#483C32',
+      'Black': '#030303',
+      'White': '#FFFFFF',
+    },
+    extend: {
+      fontFamily: {
+        'Urbanist': ['Urbanist', 'sans-serif'],
+        'Gravitica': ["Gravitica", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
