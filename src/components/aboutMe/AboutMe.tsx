340c96d4e58abb4616b958ff9da1d13c28417426
Shahabas Paramban <shahabaspm500@gmail.com>
2023-11-29 15:29:29 +0530

    AboutMe - Updates

--- src/components/aboutMe/AboutMe.tsx
+++ src/components/aboutMe/AboutMe.tsx
@@ -1,8 +1,9 @@
-import Me from "../../assets/images/me.jpg";
+import Me from "../../assets/images/me.svg";
+
 const AboutMe = () => {
   return (
     <div>
-      <div className="grid grid-cols-2 pt-10">
+      <div className="grid grid-cols-2 pt-10 pr-8">
         <div>
           <div className="text-TertiaryColor font-[GilroySemiBold] text-9xl">
             FREELANCE
@@ -14,7 +15,7 @@ const AboutMe = () => {
         {/* image */}
         <div>
           <div className="flex justify-center items-center bg-gradient-to-t from-PrimaryColor">
-            <img src={Me} alt="Image" width={"405%"} className="flex items-center justify-center " />
+            <img src={Me} alt="Image" width={"405%"} className="flex items-center justify-center" />
           </div>
           <p className="absolute bottom-[12%] text-sm text-center font-[GilroySemiBold] text-SecondaryColor pr-8">
             Welcome to my portfolio! I'm a versatile software developer specializing in creating powerful applications, sleek websites, and captivating designs. With a blend of technical expertise and a keen eye for aesthetics, I bring ideas to life with seamless functionality and striking visuals. Explore my portfolio to see how I can turn concepts into impressive digital solutions!
