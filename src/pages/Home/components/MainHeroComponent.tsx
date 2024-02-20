65c01033470629d1bc8df70eede7963279671522
Shahabas Paramban <shahabaspm500@gmail.com>
2024-02-20 12:23:56 +0530

    Working on the responsiveness in main hero section.

--- src/pages/Home/components/MainHeroComponent.tsx
+++ src/pages/Home/components/MainHeroComponent.tsx
@@ -1,6 +1,4 @@
-import ExclaramationIcon from "../../../assets/images/exclamaration_ic.svg";
-import RedUnderlineIcon from "../../../assets/images/red_underline_ic.svg";
-import BackgroundPattern from "../../../assets/images/mainhero-pattern.png";
+
 import PersonImage from "../../../assets/images/person.png";
 import ArrowIcon from "../../../assets/images/arrow2_ic.svg"
 import Insta from "../../../assets/images/insta_ic.svg";
@@ -58,11 +56,11 @@ const MainHeroComponent = () => {
     <div className="h-auto">
       {/* <img src={BackgroundPattern} width={"80%"} alt="" className="translate-x-[50%] -translate-y-[20%]" /> */}
 
-      <div className="px-[15%] text-center leading-[120%] flex items-center justify-center w-full font-[cabinetExtrabold] text-[48px] text-Black">
+      <div className="px-[15%] text-center leading-[120%] flex items-center justify-center w-full font-[cabinetExtrabold] text-[32px] lg:text-[48px] text-Black">
         Develop robust solutions with software development professionals.
       </div>
 
-      <div className="flex items-center justify-center w-full font-[GilroySemiBold] text-[16px] text-TertiaryColor">
+      <div className="flex items-center justify-center text-center w-full font-[GilroySemiBold] text-[16px] text-TertiaryColor">
         Minimize risks and maximize rewards by hiring professionals for your critical project.
       </div>
 
@@ -77,28 +75,32 @@ const MainHeroComponent = () => {
       </div> */}
 
 
-      <div className="grid grid-cols-4">
+      <div className="grid lg:grid-cols-4 ">
 
         {/* Person image */}
-        <div className="top-0 w-[50vh] mt-[40px]">
+        <div className="top-0 hidden lg:block md:w-[35vh] lg:w-[50vh] mt-[40px] ">
           <img src={PersonImage} alt="Person" />
         </div>
 
         <div className="col-span-3">
 
           {/* image and arrow */}
-          <div className="flex items-start justify-start">
-            <img src={ArrowIcon} alt="" className="-translate-x-16" /><div className="mt-[40px] bg-PrimaryColor w-[25%] px-8 py-4 rounded-[50px] text-center font-[GilroySemiBold] text-[20px] text-SecondaryColor cursor-pointer hover:-translate-y-2 duration-500 hover:shadow-lg">
+          <div className="flex items-start justify-center lg:justify-start">
+            <div className="sm:hidden md:hidden lg:block">
+              <img src={ArrowIcon} alt="" className="-translate-x-16" />
+            </div>
+            <div className="mt-[40px] bg-PrimaryColor w-auto lg:w-[25%] text-center px-4 py-4 rounded-[50px] font-[GilroySemiBold] text-[20px] text-SecondaryColor cursor-pointer hover:-translate-y-2 duration-500 hover:shadow-lg">
               Start a chat now
             </div>
           </div>
 
           {/* Introduction */}
-          <div className="text-[20px] leading-[25px] pr-[10%] text-Black font-[GilroyMedium]">
+          <div className="text-[20px] leading-[25px] px-[5%] lg:px-[0] xl:pl-[5%] mt-[10%] lg:mt-[0] text-center lg:text-left lg:pr-[10%] text-Black font-[GilroyMedium] lg:ml-[12%] xl:ml-[0%]">
             Hey there! I'm<span className="font-[GilroyExtraBold]"> Shahabas Paramban</span>, a freelance software developer based in <span className="font-[GilroyExtraBold]">Kerala, India</span>.  Driven by innovation, I tackle projects across <span className="font-[GilroyExtraBold] text-PrimaryColor">web development, app development, and UI/UX design</span>. A proud winner of <span className="font-[GilroyExtraBold]">2nd place at the 2023 National Hackathon</span>, I collaborate with a <span className="font-[GilroyExtraBold] text-PrimaryColor">team of 5 other developers</span> to bring your digital dreams to life.  Let's connect and create something amazing!
           </div>
+
           {/* social Media icons */}
-          <div className=" flex items-center justify-start mt-[8%]">
+          <div className=" flex items-center justify-center lg:justify-start mt-[8%] xl:pl-[5%] lg:ml-[12%] xl:ml-[0%]">
             {socialMediaIcons.map((items: any, index: number) => {
               return (
                 <div key={index} className="mr-[2%]">
@@ -111,20 +113,20 @@ const MainHeroComponent = () => {
           </div>
 
           {/* Paragraph */}
-          <div className="grid grid-cols-2 mt-[2%]">
-            <div className="text-[16px] font-[GilroyMedium] text-Black leading-[20px]">
+          <div className="px-[3%] lg:px-[0] grid grid-cols-6 mt-[5%] lg:mt-[2%] lg:ml-[12%] xl:pl-[5%] xl:ml-[0%]">
+            <div className="col-span-5 lg:flex-none flex justify-center lg:justify-start lg:col-span-0 text-[16px] text-center lg:text-left font-[GilroyMedium] text-Black leading-[20px] ">
               Achieved a 100% success rate on 20+ software projects, demonstrating my expertise our web and application development.
             </div>
             <div className="flex justify-end mr-[5%]">
-              <img src={AsteroidIcons} alt="icon" />
+              <img src={AsteroidIcons} width={"20%"} alt="icon" />
             </div>
           </div>
 
           {/* Partner Logo */}
-          <div className="flex items-center ml-[5%] mt-[136px]">
+          <div className="flex items-center lg:justify-normal justify-center lg:ml-[5%] mt-[100px] lg:mt-[136px]">
             {PartnerLogo.map((logo: any, index: number) => {
               return (
-                <div key={index} className="">
+                <div key={index} className="flex items-center justify-center">
                   <img src={logo.Logo} width={"50%"} alt="partners logo" />
                 </div>
               );
